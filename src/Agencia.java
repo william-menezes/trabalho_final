6d0035d1bf192a1caa913734ad5d4df4821b4525
manuelaolirs <https://github.com/manuelaolirs/poo1_t3_manuela.git>
2023-01-31 00:17:53 -0300

    por hoje eh isso, amanha continuo!

--- src/Agencia.java
+++ src/Agencia.java
@@ -1,2 +1,9 @@
+import endereco.Endereco;
+import pessoa.Gerente;
+
 public class Agencia {
+    protected int numero;
+    protected String nome;
+    protected Endereco endereco;
+    protected Gerente gerente;
 }
--- src/ClienteConta.java
+++ src/ClienteConta.java
@@ -1,2 +1,7 @@
+import conta.Conta;
+import pessoa.Cliente;
+
 public class ClienteConta {
+    protected Conta conta;
+    protected Cliente[] clientes = new Cliente[2];
 }
